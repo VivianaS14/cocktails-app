c8e827bc231690230a73b655f6cc32ea5e115664
VivianaS14 <vasorianp@gmail.com>
2023-02-09 19:37:41 -0500

    feat: styled login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import logo from "../assets/images/img-logo.png";
+import { Person, Key } from "@mui/icons-material";
+import { grey } from "@mui/material/colors";
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  if (isLoading)
+    return (
+      <div className="Login">
+        <div className="Login__image">
+          <img src={logo} alt="Cocktail Logo" width="200px" />
+        </div>
+      </div>
+    );
   return (
-    <div>
-      <h3>Login</h3>
+    <div className="Login">
+      <div className="Login__image">
+        <img src={logo} alt="Cocktail Logo" width="200px" />
+      </div>
+      <div className="Login__form">
+        <h2>Iniciar Sesión</h2>
+        <form>
+          <label htmlFor="email">
+            <span>
+              <Person sx={{ color: grey[800], fontSize: 45 }} />
+            </span>
+            <input type="text" name="email" />
+          </label>
+          <label htmlFor="password">
+            <span>
+              <Key sx={{ color: grey[800], fontSize: 45 }} />
+            </span>
+            <input type="password" name="password" />
+          </label>
+        </form>
+        <div className="Login__button">
+          <button type="submit">Iniciar Sesión</button>
+        </div>
+      </div>
+      <div className="Login__footer">
+        <p>
+          ¿Aun no tienes una cuenta? <span>¡Registrate!</span>
+        </p>
+      </div>
     </div>
   );
 };
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,24 +8,28 @@ import Table from "../pages/Table";
 import Admin from "../pages/Admin";
 import Order from "../pages/Order";
 import Invoice from "../pages/Invoice";
+import Menu from "../pages/Menu";
+import Stock from "../pages/Stock";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Layaout />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/table/:id" element={<Table />} />
-        <Route path="/home/table/order/:id" element={<Order />} />
-        <Route path="/home/table/invoice/:id" element={<Invoice />} />
-      </Route>
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/admin/home" element={<Layaout />}>
-        <Route path="/admin/home/menu" element={<Menu />} />
-        <Route path="/admin/home/stock" element={<Stock />} />
-      </Route>
-    </Routes>
+    <div className="Main">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Layaout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/table/:id" element={<Table />} />
+          <Route path="/home/table/order/:id" element={<Order />} />
+          <Route path="/home/table/invoice/:id" element={<Invoice />} />
+        </Route>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/home" element={<Layaout />}>
+          <Route path="/admin/home/menu" element={<Menu />} />
+          <Route path="/admin/home/stock" element={<Stock />} />
+        </Route>
+      </Routes>
+    </div>
   );
 };
 
